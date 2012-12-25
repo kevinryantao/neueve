23074a54ebb1ee7ec5ef90a3a7fb6fd21963a0bb
gesarlhamo80@gmail.com <gesarlhamo80@gmail.com>
2012-12-25 17:06:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lavax::Application.config.secret_token = '1149491451937346a24711ba1ffe275a0839e556480af79fa638cf78e8c437ed358f81939461dc3fdedebe280b4b20c80fcf76036ea859488f1334a5c1f943fa'
