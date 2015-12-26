5122f62c8b3bddb88111e7c1800f4c3e141d6278
Kevin Tao <kevin.ryan.tao@gmail.com>
2015-12-25 18:28:17 -0600

    Fixed log level warnings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,4 +33,6 @@ Lavax::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug                           = true
+
+  config.log_level = :debug
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ Lavax::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.log_level = :debug
 end
