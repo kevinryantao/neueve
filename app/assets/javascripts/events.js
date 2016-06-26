4122bdccc9532527e5ab75b9315d73b71c618564
Kevin Tao <kevin.ryan.tao@gmail.com>
2016-06-26 10:50:43 -0500

    Add Facebook Event tracking

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -3,29 +3,34 @@
  * Date: 7/6/14
  * Time: 1:13 AM
  *
- * Event Tracking with Google Analytics and Bing.
+ * Event Tracking with Google Analytics and Bing and Facebook Standard Events.
  * https://developers.google.com/analytics/devguides/collection/analyticsjs/events
  * http://help.bingads.microsoft.com/apex.aspx?product=bing_ads&market=en&sku=&query=hlp_ba_proc_uet.htm&view=summary_reportinggoals&inst=1
+ * https://www.facebook.com/business/help/402791146561655
  */
 
 $('.view-cart').on('click', function () {
     ga('send', 'event', 'button', 'click', 'View Cart');
+    fbq('track', 'InitiateCheckout');
 });
 
 $('.add-to-cart').on('click', function () {
     ga('send', 'event', 'button', 'click', 'Add To Cart ' + this.id);
-    var value = 50;
+    var value = 52;
     if (this.id == 'neueve-cream') {
-        value = 33;
+        value = 35;
     }
     var uetq = uetq || [];
     uetq.push({'ec': 'button', 'ea': 'click', 'el': 'Add To Cart ' + this.id, 'ev': value });
+    fbq('track', 'AddToCart');
 });
 
 $('.subscribe-button').on('click', function () {
     ga('send', 'event', 'button', 'click', 'Subscribe');
     var uetq = uetq || [];
-    uetq.push({'ec': 'button', 'ea': 'click', 'el': 'Subscribe', 'ev': 50 });
+    uetq.push({'ec': 'button', 'ea': 'click', 'el': 'Subscribe', 'ev': 52 });
+    // Send to Facebook Pixel
+    fbq('track', 'Purchase', {value: 52, currency: 'USD'});
 });
 
 $('a.amazon-review').on('click', function () {
