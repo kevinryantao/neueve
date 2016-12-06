4509e7e09ee98fd22364ad3ffda6d66e7b2ec568
Kevin Tao <kevin.ryan.tao@gmail.com>
2016-12-06 13:47:57 -0600

    Upgraded all Review Links, Added Associates Links to About Page, Upgraded Look of Testimonials

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,20 @@
 module StaticPagesHelper
+
+  def create_url(base_url, path_array, query_params_hash, anchor)
+    url = base_url
+    if !url.ends_with?('/')
+      url = url + '/'
+    end
+    for path in path_array
+      url = url + path + '/'
+    end
+    url = url + '?'
+    for (key, value) in query_params_hash
+      url = url + key.to_s + '=' + value.to_s + '&'
+    end
+    if anchor.present?
+      url = url + '#' + anchor.to_s
+    end
+    url
+  end
 end
