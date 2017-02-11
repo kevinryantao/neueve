995014505b046156aaa099422a55cc0f557b2e13
Kevin Tao <kevin.ryan.tao@gmail.com>
2017-02-11 08:57:30 -0600

    FAQ update -
    1. removed Coca Cola
    2. Added new question
    3. Revised a question

--- app/helpers/faq_helper.rb
+++ app/helpers/faq_helper.rb
@@ -4,7 +4,7 @@ require 'objects/faq_section'
 
 module FaqHelper
 
-  INGREDIENTS_SECTION = FaqSection.new('NeuEve Ingredients: ',[
+  INGREDIENTS_SECTION = FaqSection.new('NeuEve Ingredients: ', [
       FaqQuestion.new('proprietaryBlend', "I asked my doctor whether he could recommend me to use NeuEve, but he wants to know what secret ingredients in your proprietary blend are before he can give recommendation. So, could you tell me what ingredients are in your proprietary blend?"),
       FaqQuestion.new('silkSilverGoldDifferences', "What are the <strong>differences</strong> among the Silk, Silver and Gold formulas?"),
       FaqQuestion.new('silkSilverGoldIngredientDifferences', "The ingredients listed are all the same among the Silk, Silver and Gold. <strong>Are there
@@ -16,7 +16,7 @@ module FaqHelper
       FaqQuestion.new('tropicalTraditions', "The palm oil industry is destroying rainforests. For the sake of the environment and wildlife, please stop using palm oil.")
   ])
 
-  APPLICATION_SECTION = FaqSection.new('NeuEve Application: ',[
+  APPLICATION_SECTION = FaqSection.new('NeuEve Application: ', [
       FaqQuestion.new('supplements', "Can I take some <strong>supplements</strong> to help NeuEve work better?"),
       FaqQuestion.new('whySupplements', "Why would I need to supplement with other things if NeuEve works?"),
       FaqQuestion.new('dryEyeMouth', "I have tried NeuEve suppositories, but I still feel dry. Also, <strong>my eyes and mouth also
@@ -38,7 +38,7 @@ module FaqHelper
       FaqQuestion.new('still_dry', "I have had many years of vaginal atrophy. I have tried the NeuEve silver for 4 months. It worked a little but I still feel dry like a sand paper. Is there anything else that can help NeuEve work better?")
   ])
 
-  BV_SECTION = FaqSection.new('BV/Infection: ',[
+  BV_SECTION = FaqSection.new('BV/Infection: ', [
       FaqQuestion.new('otherInfections', "Since NeuEve can eliminate odor caused by <strong>BV</strong>, can it take care of other <strong>vaginal
         infections</strong>?"),
       FaqQuestion.new('newInfection', "I have a long distance new boyfriend and we had intimacy lately after using
@@ -56,7 +56,7 @@ which product would be best to use after a seven day round of antibiotic cream?"
 
   ])
 
-  COMMON_KNOWLEDGE_SECTION = FaqSection.new('Common Knowledge: ',[
+  COMMON_KNOWLEDGE_SECTION = FaqSection.new('Common Knowledge: ', [
       FaqQuestion.new('vaginalDrynessAndOdor', "Why do women suffer from <strong>vaginal dryness and odor</strong>?"),
       FaqQuestion.new('contraceptive', "Do NeuEve vaginal suppositories provide <strong>contraceptive protection</strong>?"),
       FaqQuestion.new('condoms', "Does the use of NeuEve vaginal suppositories influence the effect of <strong>condoms</strong>?"),
@@ -66,18 +66,18 @@ which product would be best to use after a seven day round of antibiotic cream?"
         suppositories if <strong>pregnant.</strong> My concern is if <strong>I were to conceive and not know yet I was
         pregnant</strong> and be using the suppositories I would not want to endanger my child."),
       FaqQuestion.new('herpes', "After using NeuEve for a day, my old <strong>herpes</strong> flares up. Does NeuEve contain L-arginine that could trigger herpes to flare up?"),
-      FaqQuestion.new('hrt', "I am on the hormone replacement therapy (HRT). I would like to quit it and use an alternative natural product like NeuEve to manage vaginal dryness. Can NeuEve compensate the loss of HRT?")
-
+      FaqQuestion.new('hrt', "I am on the hormone replacement therapy (HRT). I would like to quit it and use an alternative natural product like NeuEve to manage vaginal dryness. Can NeuEve compensate the loss of HRT?"),
+      FaqQuestion.new('nothingBackOut', "I have just begun to use the silk suppositories with the applicator. I tried your product once before without the applicator, but the insertion was not deep and much of the dissolved suppository came out quickly. <strong>Now, with the applicator, I have a new concern: nothing comes back out!</strong> Is it normal that the entire contents of the suppository, even the wax, could be absorbed by my vaginal wall?")
   ])
 
-  COST_SECTION = FaqSection.new('Cost: ',[
+  COST_SECTION = FaqSection.new('Cost: ', [
       FaqQuestion.new('whyExpensive', "NeuEve works great, but why is it so expensive?"),
       FaqQuestion.new('jelly', "<strong>Lubricant</strong> is cheap. How is NeuEve better than <strong>Jelly</strong>?"),
       FaqQuestion.new('vitamin_e', "The <strong>vitamin E</strong> suppository is cheap and it can make me feel comfortable. NeuEve is more expensive. Could you explain why it is better than the vitamin E suppository?"),
       FaqQuestion.new('subscription_cream', "I want to order NeuEve Silver Suppositories for subscription and also to buy a vaginal cream, but I cannot put these two items into one shopping cart to get the free shipping discount. Could you show me how?")
   ])
 
-  OTHER_SECTION = FaqSection.new('Other: ',[
+  OTHER_SECTION = FaqSection.new('Other: ', [
       FaqQuestion.new('changeSubscription', "How can I change my subscription from Silver to Gold formula?"),
       FaqQuestion.new('hotClimate', "I live in a very hot climate. Will the effectiveness of the product be
         compromised if the whole package is stored in the refrigerator? Or, would
