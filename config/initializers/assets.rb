be34b6334354a0b9e5dd8780dd07ce3dc4fb3377
Kevin Tao <kevin.ryan.tao@gmail.com>
2018-04-18 10:38:17 -0500

    Switched to red and green Font-Awesome icons (instead of Bootstrap). Updated to Font-Awesome 5

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
 Rails.application.config.assets.precompile += ['jquery.js',
-                                               'email_styles/style.css']
+                                               'email_styles/style.css',
+                                               'fontawesome-all.js'
+]
