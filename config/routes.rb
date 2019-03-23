aea6fed6c531567b82e9dbd6a97c0ae142a96fe1
Kevin Tao <kevin.ryan.tao@gmail.com>
2019-03-23 12:51:07 -0500

    Added Prempro comparison page

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,9 @@ Lavax::Application.routes.draw do
 
   match '/search' => 'static_pages#search', :via => :get
 
+  # Alternative Paths
   match '/premarin-cream-alternative' => 'alternatives#premarin_cream', :via => :get
+  match '/prempro-alternative' => 'alternatives#prempro', :via => :get
 
   # Immunome Research Paths
   match '/content/1/1/1' => 'immunome_research#1_1_1', :via => :get
