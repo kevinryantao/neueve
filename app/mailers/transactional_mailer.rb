0ff87c185296ca49bc84515588c7d0ba744a2ef7
Kevin Tao <kevin.ryan.tao@gmail.com>
2021-09-15 00:41:22 -0500

    Added Applicator Upsell.
    Reworked Dmannose and Vuvatech upsells.

--- app/mailers/transactional_mailer.rb
+++ app/mailers/transactional_mailer.rb
@@ -6,6 +6,8 @@ class TransactionalMailer < ApplicationMailer
 
     @is_bv_user = is_bv_user(pdt_hash)
 
+    @is_applicator_user = is_applicator_user(pdt_hash)
+
     payer_email = pdt_hash["payer_email"]
 
     mail(to: payer_email, subject: "NeuEve Order Confirmation (##{@pdt_hash["txn_id"]})")
@@ -13,17 +15,32 @@ class TransactionalMailer < ApplicationMailer
 
   def is_bv_user(pdt_hash)
     (1..(pdt_hash["num_cart_items"].to_i)).each do |i|
-      if (pdt_hash["item_name#{i}"].include? "BV") || (pdt_hash["item_name#{i}"].include? "Finisher")
+      if (pdt_hash["item_number#{i}"] == "bv-clearing-kit") || (pdt_hash["item_number#{i}"] == "finisher")
         return true
       end
     end
     false
   end
 
+  def is_applicator_user(pdt_hash)
+    (1..(pdt_hash["num_cart_items"].to_i)).each do |i|
+      if pdt_hash["item_number#{i}"] == 'applicator'
+        return true
+      end
+    end
+    false
+  end
+
+
   def subscription_email(pdt_hash)
     @pdt_hash = pdt_hash
 
     payer_email = pdt_hash["payer_email"]
+
+    @is_bv_user = false
+    if pdt_hash['item_number'] == 'bv-clearing-kit' || pdt_hash['item_number'] == 'finisher'
+      @is_bv_user = true
+    end
 
     mail(to: payer_email, subject: "NeuEve Subscription Confirmation (##{@pdt_hash["subscr_id"]})")
   end
