1fc63748c38f30db078f07dbbf3aa05c03f946a3
Kevin Tao <kevin.ryan.tao@gmail.com>
2021-09-29 01:45:50 -0500

    Added Add to Cart Tracking for everything

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -9,30 +9,150 @@
  * https://www.facebook.com/business/help/402791146561655
  */
 
+// Event snippet for Add to cart conversion page
+// In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button.
+ function gtag_report_conversion(url) {
+   var callback = function () {
+     if (typeof(url) != 'undefined') {
+       window.location = url;
+     }
+   };
+   gtag('event', 'conversion', {
+     'send_to': 'AW-1039056375/OMbTCMj7lvgCEPf7uu8D',
+     'event_callback': callback
+   });
+   return false;
+ }
+
 $('.view-cart').on('click', function () {
   ga('send', 'event', 'button', 'click', 'View Cart');
   fbq('track', 'InitiateCheckout');
 });
 
 $('.add-to-cart').on('click', function () {
-  ga('send', 'event', 'button', 'click', 'Add To Cart ' + this.id);
-  var value = 55;
-  if (this.id == 'neueve-cream') {
-    value = 38;
-  }
-  if (this.id == 'bv-clearing-kit') {
-    value = 38;
-  }
-  var uetq = uetq || [];
-  uetq.push({'ec': 'button', 'ea': 'click', 'el': 'Add To Cart ' + this.id, 'ev': value});
-  fbq('track', 'AddToCart');
+
+  gtag_report_conversion();
+
+  // Google Adwords Add to Cart support.google.com/google-ads/answer/7305793
+  gtag('event', 'add_to_cart', {
+    'send_to': 'AW-1039056375',
+    'items': [{
+      'id': this.data('sku'),
+      'google_business_vertical': 'retail'
+    }],
+    'ecomm_prodid': this.data('sku')
+  });
+
+  
+  // BING CONVERSION TRACKING https://help.ads.microsoft.com/#apex/ads/en/60118/-1
+  window.uetq = window.uetq || [];
+  window.uetq.push('event', 'add_to_cart', {
+      'ecomm_prodid': [this.data('sku')],
+      'currency': 'USD',
+      'items': [
+        {
+          'id': this.data('sku'),
+          'quantity': 1
+        }
+      ]
+    }
+  );
+
+  // Facebook Add to Cart
+  fbq('track', 'AddToCart', {
+    content_type: 'product',
+    content_ids: [this.data('sku')],
+    value: 0.50,
+    currency: 'USD'});
+
+  // https://help.pinterest.com/en/business/article/add-event-codes
+  pintrk('track', 'addtocart', {
+    product_id: this.data('sku'),
+    order_quantity: 1,
+    currency: 'USD'
+  });
+
+  // Google Analytics 4 https://developers.google.com/analytics/devguides/collection/ga4/reference/events#add_to_cart
+  gtag("event", "add_to_cart", {
+    currency: "USD",
+    items: [
+      {
+        item_id: this.data('sku'),
+        quantity: 1
+      }
+    ]
+  });
+
+  // Google Analytics UA analytics.js https://developers.google.com/analytics/devguides/collection/analyticsjs/enhanced-ecommerce#add-remove-cart
+  ga('ec:addProduct', {
+    'id': this.data('sku'),
+    'quantity': 1
+  });
+  ga('ec:setAction', 'add');
+  ga('send', 'event', 'UX', 'click', 'add to cart');     // Send data using an event.
 });
 
 $('.subscribe-button').on('click', function () {
-  ga('send', 'event', 'button', 'click', 'Subscribe');
-  var uetq = uetq || [];
-  uetq.push({'ec': 'button', 'ea': 'click', 'el': 'Subscribe', 'ev': 52});
-  fbq('track', 'AddToCart');
+  gtag_report_conversion();
+
+  // Google Adwords Add to Cart support.google.com/google-ads/answer/7305793
+  gtag('event', 'add_to_cart', {
+    'send_to': 'AW-1039056375',
+    'items': [{
+      'id': this.data('sku'),
+      'google_business_vertical': 'retail'
+    }],
+    'ecomm_prodid': this.data('sku')
+  });
+
+
+  // BING CONVERSION TRACKING https://help.ads.microsoft.com/#apex/ads/en/60118/-1
+  window.uetq = window.uetq || [];
+  window.uetq.push('event', 'add_to_cart', {
+      'ecomm_prodid': [this.data('sku')],
+      'currency': 'USD',
+      'items': [
+        {
+          'id': this.data('sku'),
+          'quantity': 1
+        }
+      ]
+    }
+  );
+
+  // Facebook Add to Cart
+  fbq('track', 'AddToCart', {
+    content_type: 'product',
+    content_ids: [this.data('sku')],
+    value: 0.50,
+    currency: 'USD'});
+
+  // https://help.pinterest.com/en/business/article/add-event-codes
+  pintrk('track', 'addtocart', {
+    product_id: this.data('sku'),
+    order_quantity: 1,
+    currency: 'USD'
+  });
+
+  // Google Analytics 4 https://developers.google.com/analytics/devguides/collection/ga4/reference/events#add_to_cart
+  gtag("event", "add_to_cart", {
+    currency: "USD",
+    items: [
+      {
+        item_id: this.data('sku'),
+        quantity: 1
+      }
+    ]
+  });
+
+  // Google Analytics UA analytics.js https://developers.google.com/analytics/devguides/collection/analyticsjs/enhanced-ecommerce#add-remove-cart
+  ga('ec:addProduct', {
+    'id': this.data('sku'),
+    'quantity': 1
+  });
+  ga('ec:setAction', 'add');
+  ga('send', 'event', 'UX', 'click', 'add to cart');     // Send data using an event.
+
 });
 
 $('a.amazon-review').on('click', function () {
